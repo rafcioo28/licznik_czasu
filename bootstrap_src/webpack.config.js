66ea4ed925c36ff709283442cde881eb645a6851
Rafał Herszkowicz <rafal.herszkowicz@outlook.com>
2020-06-26 14:29:20 +0200

    Templatka edycji osoby

--- bootstrap_src/webpack.config.js
+++ bootstrap_src/webpack.config.js
@@ -8,35 +8,32 @@ module.exports = {
     path: path.resolve(__dirname, '..', 'rodzina', 'static', 'js')
   },
   module: {
-    rules: [
-      {
-        test: /\.(scss)$/,
-        use: [
-          {
-            // Adds CSS to the DOM by injecting a `<style>` tag
-            loader: 'style-loader'
-          },
-          {
-            // Interprets `@import` and `url()` like `import/require()` and will resolve them
-            loader: 'css-loader'
-          },
-          {
-            // Loader for webpack to process CSS with PostCSS
-            loader: 'postcss-loader',
-            options: {
-              plugins: function () {
-                return [
-                  require('autoprefixer')
-                ];
-              }
+    rules: [{
+      test: /\.(scss)$/,
+      use: [{
+          // Adds CSS to the DOM by injecting a `<style>` tag
+          loader: 'style-loader'
+        },
+        {
+          // Interprets `@import` and `url()` like `import/require()` and will resolve them
+          loader: 'css-loader'
+        },
+        {
+          // Loader for webpack to process CSS with PostCSS
+          loader: 'postcss-loader',
+          options: {
+            plugins: function () {
+              return [
+                require('autoprefixer')
+              ];
             }
-          },
-          {
-            // Loads a SASS/SCSS file and compiles it to CSS
-            loader: 'sass-loader'
           }
-        ]
-      }
-    ]
+        },
+        {
+          // Loads a SASS/SCSS file and compiles it to CSS
+          loader: 'sass-loader'
+        }
+      ]
+    }]
   }
 };
